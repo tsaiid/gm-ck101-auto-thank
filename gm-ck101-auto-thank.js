90bec0f9af0db76091a8ef2cb7bea72dd1aeea0c
I-Ta Tsai <ittsai@gmail.com>
2014-01-31 13:54:53 +0800

    fix: use $.deferred to make submitting thank precisely after loading the form.

--- gm-ck101-auto-thank.js
+++ gm-ck101-auto-thank.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name       CK101 - Auto Thank for Extra Content
 // @namespace  http://blog.tsaiid.idv.tw/project/gmscripts/ck101-auto-thank-for-extra-content/
-// @version    0.1
-// @description  Some articles on Ck101 hide the extra content and you have to "Thank" the author to get it. This script will automatically "Thank the author" at the first loading. 
+// @version    0.2
+// @description  Some articles on Ck101 hide the extra content and you have to "Thank" the author to get it. This script will automatically "Thank the author" at the first loading.
 // @match      http://ck101.com/*
 // @copyright  2013+, I-Ta Tsai (http://blog.tsaiid.idv.tw/)
 // ==/UserScript==
@@ -11,14 +11,27 @@
 var $ = unsafeWindow.jQuery;
 
 if ($('.lockThankBtn').length) {
-    var thank_btn = $('.lockThankBtn').first();
-    thank_btn
-    	.click()
-    	.delay(1000)
-        .promise()
-    	.done(function(){
-			if ($('#postform').length) {
-    			$('#postform').submit();
-			}    
-    	});
+    var thank_func = $('#post_thank').attr('onclick');
+
+    var launch_thank = function() {
+        // create a deferred object
+        var r = $.Deferred();
+
+        // do whatever you want (e.g. ajax/animations other asyc tasks)
+        $.globalEval(thank_func)
+
+        setTimeout(function() {
+            // and call `resolve` on the deferred object, once you're done
+            r.resolve();
+        }, 500);
+
+        // return the deferred object
+        return r;
+    };
+
+    launch_thank().done(function() {
+        if ($('#postform').length) {
+            $('#postform').submit();
+        }
+    });
 }
