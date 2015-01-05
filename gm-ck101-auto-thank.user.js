74d46c927c26cafb96e226f311ac94f88911b063
I-Ta Tsai <ittsai@gmail.com>
2015-01-05 23:49:23 +0800

    use required jquery and set no conflict.

--- gm-ck101-auto-thank.user.js
+++ gm-ck101-auto-thank.user.js
@@ -1,25 +1,24 @@
 // ==UserScript==
 // @name            Ck101 - Auto Thank for Extra Content
 // @namespace       http://tsai.it/project/gmscripts/ck101-auto-thank-for-extra-content/
-// @version         0.2.2
+// @version         0.2.4
 // @description     Some articles on Ck101 hide the extra content and you have to "Thank" the author to get it. This script will automatically "Thank the author" at the first loading.
-// @match           http://ck101.com/*
-// @grant           unsafeWindow
+// @include         http://ck101.com/*
 // @copyright       2013+, I-Ta Tsai (http://tsai.it/)
+// @grant           none
+// @require         http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // ==/UserScript==
 
-// Use pre-loaded jQuery by ck101.
-var $ = unsafeWindow.jQuery;
+this.$ = this.jQuery = jQuery.noConflict(true);
 
-$( document ).ready(function() {
-    if ($('.lockThankBtn').length || $('.locked').length || $('.lockThank').length) {
-        $('#post_thank').click();
+if ($('.lockThankBtn').length || $('.locked').length) {
+    $('#post_thank').click();
 
-        var intervalId = setInterval(function() {
-            if ($('#postform').length) {
-                $('#postsubmit').click();
-                clearInterval(intervalId);
-            }
-        }, 500);
-    }
-});
+    var intervalId = setInterval(function() {
+        if ($('#postform').length) {
+            $('#postsubmit').click();
+            clearInterval(intervalId);
+        }
+    }, 500);
+}
+
